de30986e2f22466c5dd0eff292d4c4310910b8ae
Randy Cheng <linusche@gmail.com>
2018-02-22 00:05:47 +0800

    callservice

--- MyAppTest/Activity/CallServiceActivity.cs
+++ MyAppTest/Activity/CallServiceActivity.cs
@@ -12,27 +12,49 @@ namespace MyAppTest
     [Activity(Label ="撥號服務")]
     public class CallServiceActivity : Activity
     {
-        string[] items;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             RequestWindowFeature(WindowFeatures.NoTitle);
-            SetContentView(Resource.Layout.CallService);
+            SetContentView(Resource.Layout.CallService2);
             //陣列宣告
-            items = new string[] { "北區調度員", "中區調度員", "南區調度員" };
-            var myAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleExpandableListItem1, items);
-
-            var mylistview = FindViewById<ListView>(Resource.Id.listView1);
-            mylistview.Adapter = myAdapter;
-            mylistview.ItemClick += listView_ItemClick;
-
+            var NorthItem = FindViewById<LinearLayout>(Resource.Id.linearLayout1);
+            NorthItem.Click += delegate {
+                Intent callIntent = new Intent(Intent.ActionDial, Android.Net.Uri.Parse("tel://0800000000"));
+                StartActivity(callIntent);
+            };
+            var CenterItem = FindViewById<LinearLayout>(Resource.Id.linearLayout2);
+            CenterItem.Click += delegate {
+                Intent callIntent = new Intent(Intent.ActionDial, Android.Net.Uri.Parse("tel://0800111111"));
+                StartActivity(callIntent);
+            };
+            var SouthItem = FindViewById<LinearLayout>(Resource.Id.linearLayout3);
+            SouthItem.Click +=delegate {
+                Intent callIntent = new Intent(Intent.ActionDial, Android.Net.Uri.Parse("tel://0800222222"));
+                StartActivity(callIntent);
+            };
         }
-        void listView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
-        {
-            //Android.Widget.Toast.MakeText(this, items[e.Position].ToString(), Android.Widget.ToastLength.Short).Show();
-            Intent callIntent = new Intent(Intent.ActionDial, Android.Net.Uri.Parse("tel://0975023826"));
-            StartActivity(callIntent);
-        }
+        //string[] items;
+        //protected override void OnCreate(Bundle bundle)
+        //{
+        //    base.OnCreate(bundle);
+        //    RequestWindowFeature(WindowFeatures.NoTitle);
+        //    SetContentView(Resource.Layout.CallService);
+        //    //陣列宣告
+        //    items = new string[] { "北區調度員", "中區調度員", "南區調度員" };
+        //    var myAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleExpandableListItem1, items);
+
+        //    var mylistview = FindViewById<ListView>(Resource.Id.listView1);
+        //    mylistview.Adapter = myAdapter;
+        //    mylistview.ItemClick += listView_ItemClick;
+
+        //}
+        //void listView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        //{
+        //    //Android.Widget.Toast.MakeText(this, items[e.Position].ToString(), Android.Widget.ToastLength.Short).Show();
+        //    Intent callIntent = new Intent(Intent.ActionDial, Android.Net.Uri.Parse("tel://0975023826"));
+        //    StartActivity(callIntent);
+        //}
     }
 }
 
--- MyAppTest/Resources/Resource.Designer.cs
+++ MyAppTest/Resources/Resource.Designer.cs
@@ -235,20 +235,20 @@ namespace MyAppTest
 			// aapt resource value: 0x7f070007
 			public const int OtherReason = 2131165191;
 			
-			// aapt resource value: 0x7f070015
-			public const int Password = 2131165205;
+			// aapt resource value: 0x7f07001a
+			public const int Password = 2131165210;
 			
-			// aapt resource value: 0x7f07000d
-			public const int Text = 2131165197;
+			// aapt resource value: 0x7f070012
+			public const int Text = 2131165202;
 			
-			// aapt resource value: 0x7f070014
-			public const int UserName = 2131165204;
+			// aapt resource value: 0x7f070019
+			public const int UserName = 2131165209;
 			
-			// aapt resource value: 0x7f070013
-			public const int account = 2131165203;
+			// aapt resource value: 0x7f070018
+			public const int account = 2131165208;
 			
-			// aapt resource value: 0x7f070027
-			public const int btnAlert = 2131165223;
+			// aapt resource value: 0x7f07002a
+			public const int btnAlert = 2131165226;
 			
 			// aapt resource value: 0x7f07000a
 			public const int btnClose = 2131165194;
@@ -256,53 +256,53 @@ namespace MyAppTest
 			// aapt resource value: 0x7f070009
 			public const int btnConfirm = 2131165193;
 			
-			// aapt resource value: 0x7f070017
-			public const int btnLogin = 2131165207;
+			// aapt resource value: 0x7f07001c
+			public const int btnLogin = 2131165212;
 			
-			// aapt resource value: 0x7f07001d
-			public const int btnState = 2131165213;
+			// aapt resource value: 0x7f070022
+			public const int btnState = 2131165218;
+			
+			// aapt resource value: 0x7f07001b
+			public const int checkBox1 = 2131165211;
+			
+			// aapt resource value: 0x7f07000d
+			public const int imageView1 = 2131165197;
+			
+			// aapt resource value: 0x7f070015
+			public const int imgUser = 2131165205;
 			
 			// aapt resource value: 0x7f070016
-			public const int checkBox1 = 2131165206;
+			public const int lblListHeader = 2131165206;
 			
-			// aapt resource value: 0x7f07000c
-			public const int imageView1 = 2131165196;
-			
-			// aapt resource value: 0x7f070010
-			public const int imgUser = 2131165200;
-			
-			// aapt resource value: 0x7f070011
-			public const int lblListHeader = 2131165201;
-			
-			// aapt resource value: 0x7f070012
-			public const int lblListItem = 2131165202;
+			// aapt resource value: 0x7f070017
+			public const int lblListItem = 2131165207;
 			
 			// aapt resource value: 0x7f070008
 			public const int linearLayout1 = 2131165192;
 			
-			// aapt resource value: 0x7f07001e
-			public const int linearLayout2 = 2131165214;
+			// aapt resource value: 0x7f07000e
+			public const int linearLayout2 = 2131165198;
 			
-			// aapt resource value: 0x7f07001f
-			public const int linearLayout3 = 2131165215;
-			
-			// aapt resource value: 0x7f070021
-			public const int linearLayout4 = 2131165217;
+			// aapt resource value: 0x7f070010
+			public const int linearLayout3 = 2131165200;
 			
 			// aapt resource value: 0x7f070024
-			public const int linearLayout5 = 2131165220;
+			public const int linearLayout4 = 2131165220;
+			
+			// aapt resource value: 0x7f070027
+			public const int linearLayout5 = 2131165223;
 			
 			// aapt resource value: 0x7f07000b
 			public const int listView1 = 2131165195;
 			
-			// aapt resource value: 0x7f070028
-			public const int listView2 = 2131165224;
+			// aapt resource value: 0x7f07002b
+			public const int listView2 = 2131165227;
 			
-			// aapt resource value: 0x7f07001c
-			public const int lvExp = 2131165212;
+			// aapt resource value: 0x7f070021
+			public const int lvExp = 2131165217;
 			
-			// aapt resource value: 0x7f070018
-			public const int main_radio = 2131165208;
+			// aapt resource value: 0x7f07001d
+			public const int main_radio = 2131165213;
 			
 			// aapt resource value: 0x7f070001
 			public const int radioButton1 = 2131165185;
@@ -322,38 +322,47 @@ namespace MyAppTest
 			// aapt resource value: 0x7f070000
 			public const int radioGroup1 = 2131165184;
 			
-			// aapt resource value: 0x7f070019
-			public const int radio_button0 = 2131165209;
+			// aapt resource value: 0x7f07001e
+			public const int radio_button0 = 2131165214;
 			
-			// aapt resource value: 0x7f07001a
-			public const int radio_button1 = 2131165210;
+			// aapt resource value: 0x7f07001f
+			public const int radio_button1 = 2131165215;
 			
-			// aapt resource value: 0x7f07001b
-			public const int radio_button2 = 2131165211;
+			// aapt resource value: 0x7f070020
+			public const int radio_button2 = 2131165216;
+			
+			// aapt resource value: 0x7f07000f
+			public const int textCenter = 2131165199;
+			
+			// aapt resource value: 0x7f07000c
+			public const int textNorth = 2131165196;
+			
+			// aapt resource value: 0x7f070011
+			public const int textSouth = 2131165201;
 			
 			// aapt resource value: 0x7f070006
 			public const int textView1 = 2131165190;
 			
-			// aapt resource value: 0x7f07000e
-			public const int textView2 = 2131165198;
+			// aapt resource value: 0x7f070013
+			public const int textView2 = 2131165203;
 			
-			// aapt resource value: 0x7f07000f
-			public const int textView3 = 2131165199;
-			
-			// aapt resource value: 0x7f070022
-			public const int textView4 = 2131165218;
+			// aapt resource value: 0x7f070014
+			public const int textView3 = 2131165204;
 			
 			// aapt resource value: 0x7f070025
-			public const int textView6 = 2131165221;
+			public const int textView4 = 2131165221;
 			
-			// aapt resource value: 0x7f070023
-			public const int txtMissionName = 2131165219;
-			
-			// aapt resource value: 0x7f070020
-			public const int txtMissionNum = 2131165216;
+			// aapt resource value: 0x7f070028
+			public const int textView6 = 2131165224;
 			
 			// aapt resource value: 0x7f070026
-			public const int txtPlateNum = 2131165222;
+			public const int txtMissionName = 2131165222;
+			
+			// aapt resource value: 0x7f070023
+			public const int txtMissionNum = 2131165219;
+			
+			// aapt resource value: 0x7f070029
+			public const int txtPlateNum = 2131165225;
 			
 			static Id()
 			{
@@ -378,31 +387,34 @@ namespace MyAppTest
 			public const int CallService = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int Customlayout = 2130903043;
+			public const int CallService2 = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int HeaderCustomLayout = 2130903044;
+			public const int Customlayout = 2130903044;
 			
 			// aapt resource value: 0x7f030005
-			public const int ListItemCustomLayout = 2130903045;
+			public const int HeaderCustomLayout = 2130903045;
 			
 			// aapt resource value: 0x7f030006
-			public const int Login = 2130903046;
+			public const int ListItemCustomLayout = 2130903046;
 			
 			// aapt resource value: 0x7f030007
-			public const int Main = 2130903047;
+			public const int Login = 2130903047;
 			
 			// aapt resource value: 0x7f030008
-			public const int Maintabs = 2130903048;
+			public const int Main = 2130903048;
 			
 			// aapt resource value: 0x7f030009
-			public const int MissionList = 2130903049;
+			public const int Maintabs = 2130903049;
 			
 			// aapt resource value: 0x7f03000a
-			public const int PointItem = 2130903050;
+			public const int MissionList = 2130903050;
 			
 			// aapt resource value: 0x7f03000b
-			public const int PointList = 2130903051;
+			public const int PointItem = 2130903051;
+			
+			// aapt resource value: 0x7f03000c
+			public const int PointList = 2130903052;
 			
 			static Layout()
 			{
